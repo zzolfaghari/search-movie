f67ae6d7fb77207b70262ab9ca5d695e3caa9a49
zara <zarazolfaghari76@gmail.com>
2020-06-27 20:53:49 +0430

    add scroll down

--- src/modules/movie-browser/movie-browser.container.js
+++ src/modules/movie-browser/movie-browser.container.js
@@ -2,15 +2,43 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {Container, Row, Col} from 'react-bootstrap';
 import {AppBar} from 'material-ui';
-// e.g. { getTopMovies, ... }
 import * as movieActions from './movie-browser.actions';
 import * as movieHelpers from './movie-browser.helpers';
 import MovieList from './movie-list/movie-list.component';
+import * as scrollHelpers from '../common/scroll.helpers';
 
 class MovieBrowser extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: 1
+    };
+    // Binds the handleScroll to this class (MovieBrowser)
+    // which provides access to MovieBrowser's props
+    // Note: You don't have to do this if you call a method
+    // directly from a lifecycle method or define an arrow function
+    this.handleScroll = this.handleScroll.bind(this);
+  }
 
   componentDidMount() {
-    this.props.getTopMovies(1);
+    window.onscroll = this.handleScroll;
+    this.props.getTopMovies(this.state.currentPage);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+    const {topMovies} = this.props;
+    if (!topMovies.isLoading) {
+      let percentageScrolled = scrollHelpers.getScrollDownPercentage(window);
+      if (percentageScrolled > .8) {
+        const nextPage = this.state.currentPage + 1;
+        this.props.getTopMovies(nextPage);
+        this.setState({currentPage: nextPage});
+      }
+    }
   }
 
   render() {
@@ -25,7 +53,7 @@ class MovieBrowser extends React.Component {
             <p>Search will go here</p>
           </Row>
           <Row>
-             <MovieList movies={movies} isLoading={topMovies.isLoading} />
+            <MovieList movies={movies} isLoading={topMovies.isLoading} />
           </Row>
         </Container>
       </div>
